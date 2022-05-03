c03a13a076afc849f17da98369a55387cc3d9ef6
KTeam-Devs <kirito234875@gmail.com>
2022-05-03 18:21:32 +0530

    main:

---
+++ src/include/lexer.h
@@ -0,0 +1,11 @@
+#ifndef K_LEXER_H
+#define K_LEXER_H
+
+typedef struct LEXER_STRUCT
+{
+    char* src;
+    char c;
+    unsigned int i;
+} lexer_T;
+
+#endif 
--- src/include/token.h
+++ src/include/token.h
@@ -7,9 +7,23 @@ typedef struct TOKEN_STRUCT
 
     enum
     {
-        TOKEN_ID
+        TOKEN_ID,
+        TOKEN_EQUALS,
+        TOKEN_LPAREN,
+        TOKEN_RPAREN,
+        TOKEN_LBRACE,
+        TOKEN_RBRACE,
+        TOKEN_COLON,
+        TOKEN_COMMA,
+        TOKEN_LT,
+        TOKEN_GT,
+        TOKEN_RIGHT_ARROW,
+        TOKEN_INT,
+        TOKEN_SEMI
     } type;
 
-}
+} token_T;
+
+token_T* init_token(char* value, int type);
 
 #endif
